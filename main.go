4f34a30c349bc1ddd89dfdb4af100c86979fb74b
firescry <49809580+firescry@users.noreply.github.com>
2023-02-11 17:51:22 +0100

    Set proper CORS headers

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"github.com/firescry/grafen/instance"
 	"github.com/firescry/grafen/webfinger"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+
+	config := cors.Config{
+		AllowAllOrigins: true,
+		AllowHeaders:    []string{"content-type"},
+	}
+	router.Use(cors.New(config))
 
 	router.GET("/.well-known/webfinger", webfinger.GetWebfinger)
 
